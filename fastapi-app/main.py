88417e6e25690f440e21445d36c650c409974747
Ubuntu <ubuntu@ip-172-31-14-64.ap-south-2.compute.internal>
2026-04-23 19:01:24 +0000

    added

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello from Jenkins FastAPI CI/CD Pipeline"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
